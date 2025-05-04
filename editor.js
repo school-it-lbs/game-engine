73c4abe15cb780c503eedfc31a2d09ca1a21f4c3
school-it-lbs <schul-it@wadokei.net>
2025-05-05 00:30:41 +0200

    v3
    * Defined Scene class
    * Changed canvas to class
    * Extracted game and animation
    * Sprite definition
    * Extracted tileset
    * World class
    * Status text

--- editor.js
+++ editor.js
@@ -1,9 +1,13 @@
 let selectedTile = -1;
 
+const SCALE = 45;
+
 let mapSizeX = document.querySelector("#map-size-x").value;
 let mapSizeY = document.querySelector("#map-size-y").value;
+const tileset = new Tileset("#tileset", 16, 0, 12);
+const painter = new CanvasPainter("canvas", mapSizeY * SCALE, mapSizeX * SCALE, tileset);
 
-
+const tileSelection = document.querySelector("#tileset");
 
 
 
@@ -30,18 +34,18 @@ let main = populateArray(-1);
 const backgroundTextarea = document.querySelector("#backgroundTextarea");
 const mainTextarea = document.querySelector("#mainTextarea");
 
-const tileSelection = document.querySelector("#tileset");
+
 const scaleFactor = 2;
 const computedStyleTileSet = getComputedStyle(tileSelection);
 tileSelection.style.width = (computedStyleTileSet.width.replace("px","") * scaleFactor) + "px";
 
 tileSelection.addEventListener("click", (e) => {
-    const selectedX = Math.floor(e.offsetX / ((TILE_SIZE + TILE_GAP) * scaleFactor));
-    const selectedY = Math.floor(e.offsetY / ((TILE_SIZE + TILE_GAP) * scaleFactor));    
+    const selectedX = Math.floor(e.offsetX / tileset.scaledTileSize(scaleFactor));
+    const selectedY = Math.floor(e.offsetY / tileset.scaledTileSize(scaleFactor));    
 
     // console.log(selectedX + "|" + selectedY);
 
-    selectedTile = selectedX + (selectedY * TILES_PER_ROW);
+    selectedTile = selectedX + (selectedY * tileset.tilesPerRow);
 });
 
 
@@ -62,21 +66,22 @@ function output(){
     mainTextarea.value = outputFormat(main);
 }
 
+
+
 function render() {
-    canvas.width = mapSizeY * SCALE;
-    canvas.height = mapSizeX * SCALE;
-    clearCanvas();
-    renderMapComplete(background, mapSizeX, mapSizeY);            
-    renderMapComplete(main, mapSizeX, mapSizeY);  
-    renderGrid(mapSizeY, mapSizeX);
+    painter.clearCanvas();
+    painter.renderMapComplete(background, mapSizeX, mapSizeY);            
+    painter.renderMapComplete(main, mapSizeX, mapSizeY);  
+    painter.renderGrid(mapSizeY, mapSizeX);
     output();
 }
+
 
 document.querySelector("canvas").addEventListener('click', (e) => {    
     const row = Math.floor(e.offsetX / SCALE);
     const col = Math.floor(e.offsetY / SCALE);
 
-    // console.log(row + "|" + col);
+    console.log(row + "|" + col);
 
     const map = document.querySelector('input[name="layer"]:checked').value == "background" ? background : main;
 
@@ -87,6 +92,8 @@ document.querySelector("canvas").addEventListener('click', (e) => {
 document.querySelector("button#update").addEventListener("click", () => {
     mapSizeX = document.querySelector("#map-size-x").value;
     mapSizeY = document.querySelector("#map-size-y").value;
+    painter.resize(mapSizeY * SCALE, mapSizeX * SCALE);
+
     background = populateArray(0);
     main = populateArray(-1);
     render();
---
+++ game.js
@@ -0,0 +1,66 @@
+const CANVAS_SIZE = 720;
+const VIEWPORT_SIZE = 15;
+const VIEWPORT_OFFSET = Math.floor(VIEWPORT_SIZE / 2);
+const SCALE = CANVAS_SIZE / VIEWPORT_SIZE;
+
+const tileset = new Tileset("#tileset", 16, 0, 12);
+const painter = new CanvasPainter("canvas", CANVAS_SIZE, CANVAS_SIZE, tileset);
+const player = new Sprite(VIEWPORT_OFFSET, VIEWPORT_OFFSET, 132);
+const world = new World();
+
+let keyDownDelay;
+
+document.addEventListener('keydown', (e) => {
+    if (e.repeat) {
+        if (performance.now() - keyDownDelay < 100) {
+            return;
+        }
+    }
+
+    keyDownDelay = performance.now();
+
+    if (e.code == 'Space') {
+        world.toggleGrid();
+        return;
+    }
+
+    const previousPositionX = player.posX;
+    const previousPositionY = player.posY;
+
+    if (e.code == 'KeyW' || e.code == 'ArrowUp') {
+        if(player.posY > 0){
+            player.moveUp();
+        }        
+    }
+
+    if (e.code == 'KeyS' || e.code == 'ArrowDown') {
+        if(player.posY < world.activeScene.mapSizeX - 1){
+            player.moveDown();
+        }        
+    }
+
+    if (e.code == 'KeyA' || e.code == 'ArrowLeft') {
+        if(player.posX > 0)
+        {
+            player.moveLeft();
+        }        
+    }
+
+    if (e.code == 'KeyD' || e.code == 'ArrowRight') {
+        if(player.posX < world.activeScene.mapSizeY - 1){
+            player.moveRight();
+        }        
+    }
+
+    if (e.code == 'KeyE') {
+        player.isInteracting = true;
+    }
+
+    // collision detection
+    const nextTile = world.activeScene.main[player.posY][player.posX];
+    if (!world.reachableTiles.includes(nextTile)) {
+        player.move(previousPositionX, previousPositionY);        
+    }
+});
+
+new GameLoop().start();
